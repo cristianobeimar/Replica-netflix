a01f54a478439c790cb2df892afcd85212ee4b8a
cristianobeimar <cristianyalande63@gmail.com>
2024-06-27 16:21:12 -0500

    nuevo camvio

--- aplicacion/imc/src/App.jsx
+++ aplicacion/imc/src/App.jsx
@@ -1,18 +1,26 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import googlelogo from '/google.svg'
+import imagen from '/icon svg.svg'
 import './App.css'
+
+import datos from './database/datos.json'
+console.log(datos)
+console.log(datos.empleado)
+console.log(datos.empleado.nombre)
+
 
 function App() {
   const [count, setCount] = useState(0)
 
-  const numero = [1,2,3,4,]
-  const empleado = {
-  "nombre": "cristian",
-  "apellido": "yalanda"
-}
+//   const numero = [1,2,3,4,]
+//   const empleado = {
+//   "nombre": "Cristian",
+//   "apellido": "Yalanda"
+// }
   return (
     <>
+    
       <div>
         <a href="https://vitejs.dev" target="_blank" onClick={() => setCount((count) => count + 1)}>
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -26,11 +34,15 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           cantidad visitas  {count}
         </button>
-        <p>
-          <code>Espero encuentres lo que estas</code> buscando
-          <p>{empleado.nombre} {empleado.apellido}</p>
-          <p>{numero[0]} {numero[1]}</p>
-        </p>
+
+        <code>Espero encuentres lo que estas</code> buscandov
+        <div>
+          <img src = {imagen} class ="logo" alt="imagen" />
+          {/* <p>{empleado.nombre} {empleado.apellido}</p>
+          <p>{numero[0]} {numero[1]}</p> */}
+          
+          <p>{datos.empleado.nombre} {datos.empleado.apellido}</p>
+        </div>
       </div>
       <p className="read-the-docs">
         has clik en los logos para abrir los buscadores
