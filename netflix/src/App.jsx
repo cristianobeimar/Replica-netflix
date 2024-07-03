4e542f6e2f15c36dcafc677e6c66d56b5766a4a4
cristianobeimar <cristianyalande63@gmail.com>
2024-07-03 16:45:12 -0500

    cambios

---
+++ netflix/src/App.jsx
@@ -0,0 +1,19 @@
+import Cabecera from "./components/header/cabecera"
+
+function App() {
+
+  return (
+    <>
+    <Cabecera />
+    <header/>
+    <section className="style.div">
+      {}
+    </section>
+      <div>
+       
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ netflix/src/components/header/imagenes/pelis.jsx
@@ -0,0 +1,147 @@
+
+    
+
+const peliculas1 = [
+    {
+      id: "1",
+      categoria: "accion",
+      peliculas: [
+          {
+            id: 1,
+            name: "titanic",
+            url: "https://es.web.img2.acsta.net/c_310_420/pictures/22/05/14/13/26/5638861.jpg"
+          },
+          {
+            id: 2,
+            name: "The Incredible Hulk",
+            url: "https://cl2.buscafs.com/www.tomatazos.com/public/uploads/images/434742/434742.jpg"
+          },
+          {
+            id: 3,
+            name: "Iron Man 2",
+            url: "https://lumiere-a.akamaihd.net/v1/images/bc0ee6e1acd3e61149bdce1d08de5c7a_2764x4096_354a7134.jpeg"
+          },
+          {
+            id: 4,
+            name: "Thor",
+            url: "https://es.web.img3.acsta.net/c_310_420/medias/nmedia/18/70/92/02/20149073.jpg"
+          },
+          {
+            id: 5,
+            name: "Captain America: The First Avenger",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcky9pcqg7PAWCtLZiTeY2MQznG5whEvfGGQ&s"
+          },
+          {
+            id: 6,
+            name: "The Avengers",
+            url: "https://pics.filmaffinity.com/watchmen-337424304-msmall.jpg"
+          },
+          {
+            id: 7,
+            name: "Iron Man 3",
+            url: "https://www.utadeo.edu.co/sites/tadeo/files/node/wysiwyg/whatsapp_image_2020-05-11_at_13.43.00.jpeg"
+          },
+          {
+            id: 8,
+            name: "Iron Man 3",
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-UYf4uz5qC2h00eDt8mRL74-3mxsN7UyiUg&s"
+          }
+        ] }
+    ]
+
+          const peliculas2 = [
+            {
+                id: "2",
+                categoria: "animada",
+                peliculas:  [
+
+                   { id: 1,
+                    name: "Iron Man 3",
+                    url: "https://lumiere-a.akamaihd.net/v1/images/encanto_ka_las_pay1_92ad7410.jpeg?region=0%2C0%2C1080%2C1350"
+                   },
+                   {
+                    id: 2,
+                    name: "Iron Man 3",
+                    url: "https://hips.hearstapps.com/es.h-cdn.co/crfes/images/ninos/ocio/peliculas-para-ver-en-familia/brave-indomable/3633391-1-esl-ES/Brave-Indomable.jpg?resize=980:*"
+                   },
+                   {
+                    id: 3,
+                    name: "Iron Man 3",
+                    url: "https://stylelovely.com/wp-content/uploads/peliculas-11-sabrina.jpg"
+                   },
+                   {
+                    id: 4,
+                    name: "Iron Man 3",
+                    url: "https://stylelovely.com/wp-content/uploads/peliculas-49-pequena_miss_sunshine.jpg"
+                   },
+                   {
+                    id: 5,
+                    name: "Iron Man 3",
+                    url: "https://stylelovely.com/wp-content/uploads/peliculas-55-cadena_perpetua.jpg"
+                   },
+                   {
+                    id: 6,
+                    name: "Iron Man 3",
+                    url: "https://stylelovely.com/wp-content/uploads/peliculas-34-cinema_paradiso.jpg"
+                   },
+                   {
+                    id: 7,
+                    name: "Iron Man 3",
+                    url: "https://pm1.aminoapps.com/7653/8925031916ad5e1710f0b9fb75c8232e8a986531r1-460-666v2_uhq.jpg"
+                   },
+                ]  
+            }
+          ]
+
+          const peliculas3 = [
+                {
+                id: "2",
+                categoria: "bariadas",
+                peliculas: [
+
+                   { id: 1,
+                    name: "Iron Man 3",
+                    url: "https://www.utadeo.edu.co/sites/tadeo/files/node/wysiwyg/whatsapp_image_2020-05-11_at_13.43.00_1.jpeg"
+                   },
+                   {
+                    id: 2,
+                    name: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2_sEkIZH58pLE0Vi0VLxUXXysP98w_wJ7dA&s"
+                   },
+                   {
+                    id: 3,
+                    name: "Iron Man 3",
+                    url: "https://www.mubis.es/media/users/6547/66789/cuales-son-las-intros-de-peliculas-que-mas-te-han-gustado-consideraremos-intro-a-aquellas-escenas-que-comienzan-antes-de-ver-el-titulo-de-la-pelicula-y-o-aquellos-creditos-antes-de-empezar-con-la-pelicula-l_cover.j"
+                   },
+                   {
+                    id: 4,
+                    name: "Iron Man 3",
+                    url: "https://www.mundoprimaria.com/wp-content/uploads/2021/11/peliculas-de-dinosaurios-para-ninos.jpg"
+                   },
+                   {
+                    id: 5,
+                    name: "Iron Man 3",
+                    url: "https://www.mundoprimaria.com/wp-content/uploads/2021/11/peliculas-infantiles.jpg"
+                   },
+                   {
+                    id: 6,
+                    name: "Iron Man 3",
+                    url: "https://www.educaciontrespuntocero.com/wp-content/uploads/2020/06/4681060_640px-e1521048043801-1.jpg"
+                   },
+                   {
+                    id: 7,
+                    name: "Iron Man 3",
+                    url: "https://decine21.com/img/upload/obras/enredados-20850/enredados-20850-c.jpg"
+                   },
+                   {
+                    id: 8,
+                    name: "Iron Man 3",
+                    url: "https://pm1.aminoapps.com/7653/03c75f4e4fe39c4fbfae6ab09d6472cd21c2cfecr1-960-1440v2_hq.jpg"
+                   },
+                ]  
+            }
+          ]
+          
+  
+  
+
+
